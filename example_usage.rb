213fe01c9f8dd55bbcfa36941b18100e3b7ef5ad
Rob <rob@idrsolutions.com>
2018-05-17 15:40:37 +0100

    remove filepath used for testing accidentally committed to repo

--- example_usage.rb
+++ example_usage.rb
@@ -3,7 +3,7 @@ require 'buildvu'
 buildvu = BuildVu.new'localhost:8080/microservice-example'
 
 # convert returns a URL (string) where you can view the converted output.
-output_url = buildvu.convert 'C:/test/general/awjune2003.pdf', 'C:\\test\\out\\'
+output_url = buildvu.convert '/path/to/input.pdf'
 puts 'Converted: ' + output_url
 
 # You can also specify a directory to download the converted output to:
